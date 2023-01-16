4d23c0dc1fcfd51097954bb2cbcd9f43e5882e86
Namratha S N <73111768+namrathasn@users.noreply.github.com>
2023-01-16 08:37:06 +0530

    new

---
+++ ExceptionPractice.java
@@ -0,0 +1,18 @@
+package exceptionPractice;
+
+public class ExceptionPractice {
+    public static void main(String[] args) {
+        try{
+        int a=10;
+        int b=0;
+        int c=a/b;
+        System.out.println(c);
+
+    }
+        catch (Exception e)
+        {
+            System.out.println(e);
+        }
+    }
+
+}
